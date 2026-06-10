4723cc4a7c847c6ac4aa6bab0bda718f21b1f2c6
salma <salmatarek200055@gmail.com>
2026-06-11 01:41:10 +0300

    fix admin api path

--- Frontend/src/pages/admin/PendingServices.jsx
+++ Frontend/src/pages/admin/PendingServices.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import * as adminService from "../../services/AdminApi";
+import * as adminService from "../../services/adminApi";
 import { getCategoryByName } from "../../data/categories";
 
 import {
